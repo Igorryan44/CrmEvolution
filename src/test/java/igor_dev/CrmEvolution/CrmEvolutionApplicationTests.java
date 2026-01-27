4802e09ea10cd963910af1a6e95013fb9ce11209
Igorryan44 <igorryanmendesalves@gmail.com>
2026-01-26 23:08:48 -0300

    Iniciando a implementação de testes unitários

---
+++ src/test/java/igor_dev/CrmEvolution/CrmEvolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package igor_dev.CrmEvolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmEvolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/igor_dev/CrmEvolution/service/LeadServiceTest.java
@@ -0,0 +1,44 @@
+package igor_dev.CrmEvolution.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LeadServiceTest {
+
+    @Test
+    @DisplayName("Quando um lead for salvo com dados válidos, deve ser salvo com sucesso")
+    void saveCaseTrue() {
+    }
+
+    @Test
+    @DisplayName("Quando um lead for salvo com dados inválidos, deve lançar uma exceção")
+    void saveCaseFalse() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void updateLead() {
+    }
+
+    @Test
+    void getLeadByStatus() {
+    }
+
+    @Test
+    void updateStatus() {
+    }
+
+    @Test
+    void updateSegmento() {
+    }
+}
